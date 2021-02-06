021e257be2a8dac3ed065ad6256c7f8d1ad1b2be
boyao <>
2021-02-06 11:25:50 +0100

    solver ver1.0

--- exercise1/exercise1.m
+++ exercise1/exercise1.m
@@ -2,7 +2,7 @@ clc;
 clear all;
 
 global rhob Ta R U dt dp mu epsilon molmass_so2 molmass_so3 ...
-       molmass_o2 molmass_N2;
+       molmass_o2 molmass_N2 Tr HR_Tr;
 
 rhob = 541.42; %[kg/m^3]
 Ta = 702.6; %[K]
@@ -16,44 +16,40 @@ molmass_so2 = 15.999*2+32.066; %[kg/kmole]
 molmass_so3 = 15.999*3+32.066; %[kg/kmole]
 molmass_o2 = 15.999*2; %[kg/kmole]
 molmass_N2 = 14.007*2; %[kg/kmole]
+Tr = 699.8; %[K]
+HR_Tr = -98787.5; %[J/(molSO2)]
 
 % initial conditions
 nt0 = 54.8214; %[mol/(m2 s)]
 T0 = 777.78; %[K]
 p0 = 202650; %[pa]
 us_0 = nt0*R*T0/p0; %[m/s] initial superfacial velocity
-pso2
-
-ptot = 1.0e5; %[Pa] 
-pb0 = 0.211e5; %[Pa]
-Cp = 0.992; %[kJ/kg*K] 
-enthalpy = 1285409.0; %[kJ/kmole]
-
+pso2_0 = 22291.5; %[pa]
+pso3_0 = 1e-8; %[pa]
+po2_0 = 20265; %[pa]
+%pn2_0 = p0-pso2_0-pso3_0-po2_0; %[pa]
 
 zstart = 0; %[m]
-zend = 3; %[m]
-pA0 = 0.015e5; %[Pa]
-ptot0 = 10000;
-us0 = 2.2e-3;
+zend = 6.096; %[m]
 
 %integration span
 zspan=[zstart zend];
 
 %initial conditions
-y0=[pA0 T0 ptot0 us0];
+y0=[p0 T0 us_0 pso2_0 po2_0 pso3_0];
 
 [z,y]=ode15s(@yderiv,zspan,y0);
 
 %plot the result
 
-m = 4;
+m = 6;
 n = 1;
 nr = 1;
 
 
 subplot(m,n,nr);
 plot(z,y(:,1))
-title('Partial pressure profile')
+title('total pressure profile')
 xlabel('z [m]') 
 ylabel('p [Pa]')
 
@@ -63,19 +59,34 @@ title('Temperature profile')
 xlabel('z [m]') 
 ylabel('T [K]')
 
-
 subplot(m,n,3);
 plot(z,y(:,3))
-title('total pressure profile')
+title('superfacial velocity profile')
+xlabel('z [m]') 
+ylabel('us [m/s]')
+
+subplot(m,n,4);
+plot(z,y(:,4))
+title('SO2 pressure profile')
+xlabel('z [m]') 
+ylabel('Ptot [pa]')
+
+subplot(m,n,5);
+plot(z,y(:,5))
+title('O2 pressure profile')
+xlabel('z [m]') 
+ylabel('Ptot [pa]')
+
+subplot(m,n,6);
+plot(z,y(:,6))
+title('SO3 pressure profile')
 xlabel('z [m]') 
 ylabel('Ptot [pa]')
 
 
-subplot(m,n,4);
-plot(z,y(:,4))
-title('superfacial velocity profile')
-xlabel('z [m]') 
-ylabel('us [m/s]')
+
+
+
 
 
 
--- exercise1/yderiv.m
+++ exercise1/yderiv.m
@@ -1,27 +1,118 @@
 function dydz=yderiv(z,y)
-global rhob Ta R U dt dp mu epsilon;
-pso2 = y(1);
-po2 = y(2);
-pso3 = y(3);
-T = y(4);
-ptot = y(5);
-us = y(6);
-f=1;
-dp=0.1;
-k=1.0e-10*exp(19.837-13636.0/T)/3600.0; 
-r=k*pb0*pso2;
-rso2 = r;
-ro2 = 0.5 * r;
-rso3 = -r;
-rhog=ptot*molmass/(R*T);
-dptdz=-f*rhog*us^2/dp;
-dTdz=1.0/(us*rhog*Cp)*(enthalpy*rhob*r-4.0*U/dt*(T-Tr));
-dusdz=-us*molmass/(rhog*R)*(1/T*dptdz-ptot/(T^2)*dTdz);
-dpso2dz=-1.0/us*(molmass*ptot*rhob/rhog*r)+pso2/T*dTdz+pso2/us*dusdz;
-dpo2dz=-1.0/us*(molmass*ptot*rhob/rhog*r)+po2/T*dTdz+po2/us*dusdz;
-dpso3dz=-1.0/us*(molmass*ptot*rhob/rhog*r)+pso3/T*dTdz+pso3/us*dusdz;
+global rhob Ta R U dt dp molmass_so2 molmass_so3 ...
+       molmass_o2 molmass_N2;
+   
+ptot = y(1);
+T = y(2);
+us = y(3);
+pso2 = y(4);
+po2 = y(5);
+pso3 = y(6);
+%pn2 = y(7); 
+pn2 = ptot - pso2 - po2 -pso3;
 
-dydz=[dptdz;dTdz;dusdz;dpso2dz];
+% equation parameters
+
+Mm_coeff = Mm(pso2,pso3,po2,pn2);
+
+rhog=ptot*Mm_coeff/(R*T);
+
+f_coeff = f(Rep(rhog,us));
+
+Cpg = rho(pso2,molmass_so2,T) * CpSO2(T) ...
+    + rho(po2,molmass_o2,T) * CpO2(T) ...
+    + rho(pso3,molmass_so3,T) * CpSO3(T) ...
+    + rho(pn2,molmass_N2,T) * CpN2(T);
+enthalpy = deltaHR(T);
+
+k_coeff = k(T);
+
+rso2 = r(k_coeff,po2,pso2,pso3,Kp(T),"so2");
+rso3 = r(k_coeff,po2,pso2,pso3,Kp(T),"so3");
+ro2 = r(k_coeff,po2,pso2,pso3,Kp(T),"o2");
+
+dTdz=1.0/(us*rhog*Cpg)*(-enthalpy*rhob*rso2-4.0*U/dt*(T-Ta));
+dptdz=-f_coeff*rhog*us^2/dp; 
+dusdz=-us*Mm_coeff/(rhog*R) * (1/T*dptdz - ptot/(T^2)*dTdz);%-us/ptot*(dptdz-ptot/T*dTdz);
+dpso2dz=-pso2/us*dusdz + pso2/T*dTdz - rhob*R*T*rso2/us;
+dpo2dz=-po2/us*dusdz + po2/T*dTdz - rhob*R*T*ro2/us;
+dpso3dz=-pso3/us*dusdz + pso3/T*dTdz - rhob*R*T*rso3/us;
+%dpn2dz=-pn2/us*dusdz + pn2/T*dTdz;
+
+dydz=[dptdz;dTdz;dusdz;dpso2dz;dpo2dz;dpso3dz];
+
 end
 
 
+%% heat capacity
+function Cp = CpSO2(T)
+Cp = 30.178+ T*42.452*10^(-3) - T*T*18.218*10^(-6);
+end
+
+function Cp = CpO2(T)
+Cp = 23.995+ T*17.507*10^(-3) - T*T*6.628*10^(-6);
+end
+
+function Cp = CpSO3(T)
+Cp = 35.634+ T*71.722*10^(-3) - T*T*31.539*10^(-6);
+end
+
+function Cp = CpN2(T)
+Cp = 26.159+ T*6.615*10^(-3) - T*T*2.889^(-7);
+end
+
+%% Equilibrium constant
+function Kp_ = Kp(T)
+global R;
+Kp_ = 3.142*10^(-3)*exp(98325/(R*T)-11.24); %[Pa^(-0.5)]
+end
+
+%% Reaction rare constant
+function kRate = k(T)
+kRate = 9.8692*10^(-3)*exp(-97782/T-110.1*log(T)+848.1); %[mol(SO2)/kg(cat)*Pa]
+end
+
+%% Reaction rate
+function rrate = r(k,po2,pso2,pso3,Kp,name)
+if(name == "so2")
+        rrate = -k* sqrt(pso2/pso3) * ( po2 - (pso3/(Kp * pso2))^2 );
+end
+if(name == "so3")
+        rrate = k* sqrt(pso2/pso3) * ( po2 - (pso3/(Kp * pso2))^2 );
+end
+if(name == "o2")
+        rrate = -0.5*k* sqrt(pso2/pso3) * ( po2 - (pso3/(Kp * pso2))^2 );
+end
+end
+
+%% Reynolds number
+function Rey = Rep(rhog,us)
+global dp mu;
+Rey = rhog*us*dp/mu; %Calculating the friction factor;
+end
+
+%% Friction coefficient
+function fri = f(Reynolds)
+global epsilon;
+fri = ((1-epsilon)/epsilon^3) * (1.75+ 4.2*Reynolds^(5/6)*(1-epsilon)/Reynolds);
+end
+
+%% reaction energy/enthalpy
+function HR = deltaHR(T)
+global Tr HR_Tr;
+HR = HR_Tr + (-6.5415)* (T-Tr) + 0.5 * 0.02057 * (T^2 - Tr^2) + (-1.0011e-5)/3 * (T^3 - Tr^3);
+end
+
+%% Averaged molar mass
+function Mavg = Mm(pso2,pso3,po2,pn2)
+global molmass_so2 molmass_so3 molmass_o2 molmass_N2;
+Mavg = (pso2 * molmass_so2 + pso3 * molmass_so3 ...
+      + po2 * molmass_o2 + pn2 * molmass_N2) ...
+      /(pso2 + pso3 + po2 + pn2);
+end
+
+%% phase density
+function rhoi = rho(p,molmass,T)
+global R;
+rhoi = p * molmass / (R * T);
+end
